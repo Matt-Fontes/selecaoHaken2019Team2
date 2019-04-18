41049613e87edf020a347fbae944ba5e0e08a1ce
Guilherme Ytalo <gydaytalo@gmail.com>
2019-04-18 00:33:59 -0300

    LandingPage
    Inicio da landing page já feita certa parte de html e css, esta faltando imagens e icons, e também esta faltando a integração com o back e funções dos botões

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public')); //Dizendo aonde estao as pastas css, img e js
+
 const uri = "mongodb://localhost:27017/projetoHaken";
 
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -22,7 +24,3 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
-
-app.post('/show', (req, res) => {
-
-})
